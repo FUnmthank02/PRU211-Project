ebd94c9020fff6ff7920da17bd548ac5d5127a06
Thanhnm <88494626+FUnmthank02@users.noreply.github.com>
2023-06-29 12:54:42 +0700

    thanhnm/ Create enermies' health bar, Create enermies' attack point
    + Create enermies' health bar
    + Create enermies' attack point

---
+++ Assets/Scripts/HealthBar/GoblinHealth.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoblinHealth : MonoBehaviour
+{
+    public Slider healthSlider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxHeath(int health)
+    {
+        SetHeath(health);
+    }
+
+    public void SetHeath(int health)
+    {
+        healthSlider.value = health;
+        fill.color = gradient.Evaluate(healthSlider.normalizedValue);
+    }
+}
