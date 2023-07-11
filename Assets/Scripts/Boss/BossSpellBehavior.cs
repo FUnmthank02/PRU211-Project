07bfd06d8b762adb717e69590698477c9d9420d0
Thanhnm <88494626+FUnmthank02@users.noreply.github.com>
2023-07-11 21:29:35 +0700

    Thanhnm/Update Boss' collider, Boss' attackpoint's position, Update Spell 's script when colliding Player

--- Assets/Scripts/Boss/BossSpellBehavior.cs
+++ Assets/Scripts/Boss/BossSpellBehavior.cs
@@ -1,15 +1,23 @@
 using Assets.Scripts;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BossSpellBehavior : MonoBehaviour
 {
     Animator animator;
+    //private GameObject player;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         animator.SetTrigger("Appear");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag.Equals(Constants.player_name))
+        {
+            // this code to Damage the player
+            //player.GetComponent<PlayerHealth>().TakeDamage(attackDamage);
+        }
     }
 }
